9e1e422bcfb1769d067cd6d17d468255fbfd6d7d
cdnicoll <cdnicoll@gmail.com>
2023-01-25 14:16:10 -0800

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
-# eslint-setup
-npx package for @ocupop/eslint-config setup
+# Ocupop Linter Config
+Sets up [ESLint](https://eslint.org/) config within `npx` across projects
+
+## Setup
+Run the following `npx` command to install ESLint
+
+```
+npx @ocupop/eslint-config
+```
+
+### Options
+```
+  --version        Show version number                                 [boolean]
+  -h, --help       Show help                                           [boolean]
+  --remove, --rm   Remove eslint from application   [boolean] [default: false]
+  --upgrade, --up  Upgrade eslint config            [boolean] [default: false]
+  ```
+
+## Using
+### Files
+Once installed, the package will add a new file `.eslintrc` where we can add additional [rules](https://eslint.org/docs/rules/), or override [rules](https://eslint.org/docs/rules/) values.
+
+### Scripts
+Within `package.json` two new scrips are installed.
+```
+$ yarn checkLint  # will run eslint and validate files
+$ yarn lint       # formats all files (with specified extensions) 
+```
+
+### ESLint Extension
+By default we do not require that the eslint extension be installed. Simply running `yarn lint` will run eslint within our code base. We can install the [eslint extension](https://marketplace.visualstudio.com/items?itemName=dbaeumer.vscode-eslint) which will help format and point out errors as you develop.
+
+## Publishing
+```
+$ npm login <login with ocupop creds>
+# [ ] Make sure to push changes to git first
+# [ ] Make sure to bump the version number
+$ npm publish
+```
---
+++ index.js
@@ -0,0 +1,158 @@
+#!/usr/bin/env node
+const fs = require('fs')
+const yargs = require('yargs')
+const childProccess = require('child_process')
+
+yargs
+  .help('h')
+  .alias('h', 'help')
+  .option('remove', {
+    describe: 'Remove eslint from application',
+    type: 'boolean',
+    alias: 'rm',
+    default: false
+  })
+  .option('upgrade', {
+    describe: 'Upgrade eslint config',
+    type: 'boolean',
+    alias: 'up',
+    default: false
+  })
+
+const argv = yargs.argv
+
+// Validate we have a package json to work with. If not we can't do much
+if (!fs.existsSync('package.json')) {
+  console.error(
+    'No package.json found. Make sure you are in the project root. If no package.json exists yet, run `npm init` first.'
+  )
+  process.exit(1)
+}
+
+const lintPackage = '@ocupop/eslint-config'
+const lintPackagesToInstall = [
+  // 'eslint',
+  // 'eslint-config-airbnb',
+  // 'babel-eslint',
+  // 'eslint-plugin-jsx-a11y',
+  // 'eslint-plugin-react',
+  // 'eslint-plugin-import'
+].join(' ')
+
+const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'))
+packageJson.scripts = packageJson.scripts || {}
+
+// Write eslint config files
+const CONFIG_FILES = {
+  '.eslintrc': `\
+/**
+* @type { import("eslint").Options }
+*/
+{
+  "extends": [
+    "${lintPackage}"
+  ]
+  // Override other rules here...
+}
+`
+}
+
+/**
+ * Adds Linter to the project
+ */
+function addLinter() {
+  // Write files
+  Object.entries(CONFIG_FILES).forEach(([fileName, contents]) => {
+    if (!fs.existsSync(fileName)) {
+      fs.writeFileSync(fileName, contents, 'utf8')
+    } else {
+      console.warn(
+        `skipping over writing ${fileName} because it already exists`
+      )
+    }
+  })
+
+  packageJson.scripts.lint = "eslint '**/*.js' --ignore-pattern node_modules/"
+  packageJson.scripts.lintFix =
+    "eslint --fix '**/*.js' --ignore-pattern node_modules/"
+
+  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2), 'utf8')
+
+  // add packages to the project
+  childProccess.execSync(
+    `npm install --save-dev ${lintPackage} ${lintPackagesToInstall}`,
+    {
+      stdio: 'inherit'
+    }
+  )
+}
+
+/**
+ * Updates the linter packages
+ */
+function upgradeLinter() {
+  childProccess.execSync(`npm update ${lintPackage} ${lintPackagesToInstall}`, {
+    stdio: 'inherit'
+  })
+}
+
+/**
+ * Removes Linter from the project
+ */
+function removeLinter() {
+  // Remove files
+  Object.entries(CONFIG_FILES).forEach(([fileName, contents]) => {
+    // validate file exists
+    if (fs.existsSync(fileName)) {
+      fs.unlink(fileName, (error) => {
+        // supress the error
+      })
+    }
+  })
+
+  // clean package.json scripts
+  delete packageJson.scripts.lint
+  delete packageJson.scripts.lintFix
+
+  fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2), 'utf8')
+
+  // remove the packages
+  childProccess.execSync(`npm uninstall ${lintPackage}`, {
+    stdio: 'inherit'
+  })
+}
+
+/**
+ * Main entry point for the application
+ */
+async function init() {
+  // Check against arugments passed in
+  if (argv.remove) {
+    removeLinter()
+  } else if (argv.upgrade) {
+    upgradeLinter()
+  } else {
+    addLinter()
+  }
+  //   // validate we really want to remove everything
+  //   await inquirer
+  //     .prompt([
+  //       {
+  //         type: 'confirm',
+  //         name: 'shouldClean',
+  //         message: 'This will remove all linter configs, continue?'
+  //       }
+  //     ])
+  //     .then(value => {
+  //       if (value.shouldClean) {
+  //         removeLinter()
+  //       }
+  //     })
+  // } else if (argv.upgrade) {
+  //   upgradeLinter()
+  // } else {
+  //   addLinter()
+  // }
+}
+
+init()
